8f394ab14f142e16e70957e7a4f4f3c3a5c8e5bd
DenysDenylson <DenysDenylson@gmail.com>
2014-10-07 21:22:03 -0400

    modulo noticias agregado

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :noticia
+
   resources :mascota
 
   resources :voluntario_temporals
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141005173756) do
+ActiveRecord::Schema.define(version: 20141008001919) do
 
   create_table "mascota", force: true do |t|
     t.string   "nombre"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20141005173756) do
     t.string   "anti"
     t.string   "esterilizacion"
     t.text     "observacion"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "noticia", force: true do |t|
+    t.string   "titulo"
+    t.string   "categoria"
+    t.text     "contenido"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
